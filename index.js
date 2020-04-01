5b9b1c3b5c3509f90e2a981a31944122bfec5d5f
amal jose <amal.jose@appsintegra.com>
2020-04-01 13:16:23 +0530

    Adding 2 way relation

--- index.js
+++ index.js
@@ -12,37 +12,42 @@ const PROJECT_NAME = "tweetz";
 const keystone = new Keystone({
   name: PROJECT_NAME,
   adapter: new Adapter(),
-  onConnect: async keystone => {
-    await keystone.createItems({
-      User: [
-        { name: 'Tweetz team', email: 'team@tweetz.com', password: 'dolphins' },
-      ],
-      Tweet: [
-        {
-          content: 'Hello, Welcome to tweetz',
-          author: { where: { name: 'Tweetz team' } },
-          dateCreated: Date.now()
-        },
-      ],
-    });
-  },
+  // onConnect: async keystone => {
+  //   await keystone.createItems({
+  //     User: [
+  //       { name: 'Tweetz team', email: 'team@tweetz.com', password: 'dolphins' },
+  //     ],
+  //     Tweet: [
+  //       {
+  //         content: 'Hello, Welcome to tweetz',
+  //         author: { where: { name: 'Tweetz team' } },
+  //         dateCreated: Date.now()
+  //       },
+  //     ],
+  //   });
+  // },
 });
 
 keystone.createList('User', {
   schemaDoc: 'A list of the users',
   fields: {
-    name: { type: Text, schemaDoc: 'This is the name of the user' },
-    email: { type: Text, schemaDoc: 'This is the email id of the user' },
-    password: { type: Password, schemaDoc: 'This is the password of the user' },
+    name: { type: Text, schemaDoc: 'This is the name of the user', isRequired: true },
+    email: { type: Text, schemaDoc: 'This is the email id of the user', isRequired: true },
+    password: { type: Password, schemaDoc: 'This is the password of the user', isRequired: true },
+    tweets: {
+      type: Relationship,
+      ref: 'Tweet.author',
+      many: true,
+    },
   },
 });
 
 keystone.createList('Tweet', {
   schemaDoc: 'All tweets',
   fields: {
-    content: { type: Text, schemaDoc: 'This is the content of the tweet' },
-    dateCreated: { type: Text, schemaDoc: 'This is the time when post is created' },
-    author: { type: Relationship, ref: 'User' },
+    content: { type: Text, schemaDoc: 'This is the content of the tweet', isRequired: true },
+    dateCreated: { type: Text, schemaDoc: 'This is the time when post is created', isRequired: true },
+    author: { type: Relationship, ref: 'User.tweets', isRequired: true },
   },
 });
 
